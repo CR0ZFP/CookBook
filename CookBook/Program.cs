baefa51f6504a05094c11eaf168dc977283250c5
CR0ZFP <gergolorincz100@gmail.com>
2025-04-02 01:13:16 +0200

    Program.cs fixed

--- CookBook/Program.cs
+++ CookBook/Program.cs
@@ -1,25 +1,60 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using CookBook.Data;
+using CookBook.Logic;
+using CookBook.Logic.Dto;
+using CookBook.Data.Repositories;
+using CookBook.Logic.Dto;
+using CookBook.Logic.Logic;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using CookBook.Data;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace CookBook.Endpoint
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<CookBookContext>(options =>
+            {
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CookBookDb;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True");
+            });
+
+
+            builder.Services.AddScoped(typeof(Repository<>));
+
+            //Logic osztályok és mapper
+            builder.Services.AddScoped<DtoProvider>();
+            builder.Services.AddScoped<FoodLogic>();
+            builder.Services.AddScoped<IngredientLogic>();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
