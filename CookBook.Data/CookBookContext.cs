832ab90d15721b4598e9b897006f7017b722b3ce
CR0ZFP <gergolorincz100@gmail.com>
2025-04-01 19:31:57 +0200

    Db context created

---
+++ CookBook.Data/CookBookContext.cs
@@ -0,0 +1,44 @@
+﻿using CookBook.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Data
+{
+    class CookBookContext : IdentityDbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Ingredient> Ingedients { get; set; }
+
+        public CookBookContext (DbContextOptions<CookBookContext> opt) : base (opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FoodIngredient>()
+                .HasKey(x => new  { x.FoodId, x.IngredientId });
+
+            modelBuilder.Entity<FoodIngredient>()
+                .HasOne(x => x.Food)
+                .WithMany(y => y.FoodIngredients)
+                .HasForeignKey(x => x.FoodId);
+
+            modelBuilder.Entity<FoodIngredient>()
+                .HasOne(x => x.Ingredient)
+                .WithMany(y => y.FoodIngredients)
+                .HasForeignKey(x => x.IngredientId);
+
+           
+        }
+    }
+}
