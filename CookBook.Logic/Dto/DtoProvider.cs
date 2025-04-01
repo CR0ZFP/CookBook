f4fa8e5600f4812ba32ed1868164f67192bad11c
CR0ZFP <gergolorincz100@gmail.com>
2025-04-01 22:36:55 +0200

    Dto provider created

---
+++ CookBook.Logic/Dto/DtoProvider.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CookBook.Entities;
+using CookBook.Entities.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Logic.Dto
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            Mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Food, FoodViewDto>();
+                cfg.CreateMap<FoodCreateDto, Food>();
+            }));
+        }
+    }
+}
